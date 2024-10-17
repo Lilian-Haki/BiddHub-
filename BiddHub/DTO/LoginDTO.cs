0d19d298b5f2de423a8fe8d83fdc81a658b04821
Lilian-Haki <irungulilianhaki@gmail.com>
2024-10-17 16:45:06 +0300

    third commit

--- BiddHub/DTO/LoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BiddHub.DTO
-{
-    public class LoginDTO
-    {
-            public required string Username { get; set; }
-            public required string Password { get; set; }
-    
-    }
-}
